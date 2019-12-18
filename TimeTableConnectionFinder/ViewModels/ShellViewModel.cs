2e778a0c86e008740515bdd24f4056212a654ca4
Kithrian <pallene@protonmail.com>
2019-12-18 11:07:31 +0100

    Cleaned Project

--- TimeTableConnectionFinder/ViewModels/ShellViewModel.cs
+++ TimeTableConnectionFinder/ViewModels/ShellViewModel.cs
@@ -3,6 +3,8 @@ using System;
 namespace TimeTableConnectionFinder {
     public class ShellViewModel : Caliburn.Micro.PropertyChangedBase, IShell
     {
-        //MyBrowser.Source = new Uri("https://nunit.org/");
+        
+
+
     }
 }
